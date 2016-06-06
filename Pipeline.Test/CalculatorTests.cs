a7aaa7082cb062746dd25e9dbdae2676fc21e0da
Deon Heyns <heynsd@gmail.com>
2016-06-06 11:00:24 -0300

    initial commit for ci testing

---
+++ Pipeline.Test/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Pipeline.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Test()
+        {
+            Calculator calc = new Calculator();
+
+            int result = calc.Add(1, 2);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ Pipeline/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Pipeline
+{
+    public class Calculator
+    {
+        public int Add(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+    }
+}
